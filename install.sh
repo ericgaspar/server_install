c232bde9d322002eabe1ce4bdce7561a5c26c895
ericgaspar <junk.eg@free.fr>
2019-05-04 11:50:44 +0200

    Update install.sh
    EOF variable

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#########################################################################
-#	LEMP server for Raspberry Pi                                        #
-#	This script will install Nginx, PHP, MySQL, PHPMyAdmin              #
-#	4/5/2019                                                            #
-#########################################################################
+####################################################################################
+#	LEMP server for Raspberry Pi                                               #
+#	This script will install Nginx, PHP, MySQL, PHPMyAdmin                     #
+#	4/5/2019                                                                   #
+####################################################################################
 
 
 if [ "$(whoami)" != "root" ]; then
@@ -49,7 +49,7 @@ update-rc.d php7.0-fpm defaults
 sed -i 's/^;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' /etc/php/7.0/fpm/php.ini
 sed -i 's/# server_names_hash_bucket_size/server_names_hash_bucket_size/' /etc/nginx/nginx.conf
 
-cat > /etc/nginx/sites-enabled/default << "EOF"
+cat > /etc/nginx/sites-enabled/default <<EOF
 # Default server
 server {
 	listen 80 default_server;
@@ -58,8 +58,8 @@ server {
 	#listen 443 ssl http2 default_server;
 	#listen [::]:443 ssl http2 default_server;
 	
-	server_name www.cuboctaedre.xyz cuboctaedre.xyz;
-	root /var/www/cuboctaedre.xyz;
+	server_name www.$DOMAIN $DOMAIN;
+	root /var/www/$DOMAIN;
 	index index.php index.html index.htm default.html;
 
 	location / {
@@ -93,8 +93,8 @@ server {
     #    ssl_session_timeout 5m;
 
     # ssl
-    #    ssl_certificate /etc/letsencrypt/live/cuboctaedre.xyz/fullchain.pem;
-    #    ssl_certificate_key /etc/letsencrypt/live/cuboctaedre.xyz/privkey.pem;
+    #    ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem;
+    #    ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem;
 
 	# Enable server-side protection against BEAST attacks
     #   ssl_prefer_server_ciphers on;
